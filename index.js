5b5d43a69dbc6271b81379640372fa01920e8ddd
OuiAtichat <atichat@nexpie.com>
2020-10-16 14:58:47 +0700

    update2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const middleware = require('@line/bot-sdk').middleware;
+const line = require('@line/bot-sdk');
+const JSONParseError = require('@line/bot-sdk').JSONParseError;
+const SignatureValidationFailed = require('@line/bot-sdk').SignatureValidationFailed;
 
 const port = process.env.PORT || 4000;
 
@@ -11,13 +13,46 @@ const config = {
 	channelSecret: '52dbb515ebde7581403ee6bd82aba7c1',
 };
 
-app.post('/webhook', middleware(config), (req, res) => {
-	//   req.body.events // webhook event objects
-	//   req.body.destination // user ID of the bot (optional)
+// app.post('/webhook', line.middleware(config), (req, res) => {
+// 	//   req.body.events // webhook event objects
+// 	//   req.body.destination // user ID of the bot (optional)
 
-	console.log('req.body', req.body);
-	res.sendStatus(200);
-	// res.json(req.body.events);
+// 	console.log('req.body', req.body);
+// 	res.sendStatus(200);
+// 	// res.json(req.body.events);
+// });
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+	Promise.all(req.body.events.map(handleEvent)).then((result) => res.json(result));
 });
 
-app.listen(port);
+const client = new line.Client(config);
+function handleEvent(event) {
+	if (event.type !== 'message' || event.message.type !== 'text') {
+		return Promise.resolve(null);
+	}
+
+	return client.replyMessage(event.replyToken, {
+		type: 'text',
+		text: event.message.text,
+	});
+}
+
+app.use((err, req, res, next) => {
+	if (err instanceof SignatureValidationFailed) {
+		res.status(401).send(err.signature);
+		return;
+	} else if (err instanceof JSONParseError) {
+		res.status(400).send(err.raw);
+		return;
+	}
+	next(err); // will throw default 500
+});
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
